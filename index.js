a6c2905c8df3d0eea59e99a8c2b0785cf584ddf2
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-12-02 16:50:45 -0500

    DELETE is functioning

--- index.js
+++ index.js
@@ -53,15 +53,31 @@ server.get("/api/users/:id", (req, res) => {
     .then(user => {
       user
         ? res.status(200).json(user)
-        : res
-            .status(404)
-            .json({ error: "The user with the specified ID does not exist. " });
+        : res.status(404).json({
+            message: "The user with the specified ID does not exist. "
+          });
     })
     .catch(error => {
       console.log("error on GET /api/users/:id");
       res
         .status(500)
         .json({ error: "The user information could not be retrieved." });
+    });
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.remove(id)
+    .then(removed => {
+      removed
+        ? res.status(200).json({ message: "Successfully removed.", removed })
+        : res
+            .status(404)
+            .json({ message: "The user with specified ID does not exist." });
+    })
+    .catch(error => {
+      res.status(500).json({ error: "The user could not be removed" });
     });
 });
 
